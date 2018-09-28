83632e88bca92d7e2570de77941389d9a4b5708f
Tim Hillier <Tim.r.hillier@gmail.com>
2018-09-28 00:07:22 -0700

    Generate Data and store it in arrays.

--- src/com/company/FileIO.java
+++ src/com/company/FileIO.java
@@ -10,24 +10,61 @@ import java.util.stream.Stream;
 //Deals with the file IO of the program
 public class FileIO {
         public static String First;
+        static boolean firstTime = true;
+        static String[][] BigBrotherArray;
 
 
 
-    public static void GetInputFile(String loc)
-    {
-        try(Stream<String> st = Files.lines(Paths.get(loc)))
+    public static void GetInputFile(String loc) {
+        //come back later and try to find a better way to store and get the file.
+//        try(Stream<String> st = Files.lines(Paths.get(loc)))
+//        {
+//            //get the first element which is the number of transactions:
+//            First = st.findFirst().get();
+//            List<String> Disct;
+//
+//            System.out.println("There are " + First + " transactions");
+//            System.out.println(Disct);
+//
+//
+//        }
+//        catch(IOException e)
+//        {
+//
+//        }
+        //this is the boring way to do this/
+        try (BufferedReader br = new BufferedReader(new FileReader(loc)))
         {
-            //get the first element which is the number of transactions:
-            First = st.findFirst().get();
-            List<String> Disct;
+            String line;
+            while((line = br.readLine()) != null)
+            {
+                if(firstTime)
+                {
+                    First = line;
+                    firstTime = false;
+                    //Make Array to hold array;
+                    //God am I actually making an array of arrays??????
+                    BigBrotherArray = new String[Integer.parseInt(First)][];
 
-            System.out.println("There are " + First + " transactions");
-            System.out.println(Disct);
+                }
+                else {
+                    //Split the string and sort them into their boxes;
+                    String[] split = line.split("\\s+");
+                    String[] TempArray = new String[Integer.parseInt(split[1])];
+                    for (int i = 0; (i < Integer.parseInt(split[1])); i++) {
+                        TempArray[i] = split[i + 2];
+                    }
+                    BigBrotherArray[Integer.parseInt(split[0]) - 1] = TempArray;
+                }
+                }
 
+           // System.out.println("First: " + First);
+            System.out.println(Arrays.deepToString(BigBrotherArray));
         }
         catch(IOException e)
         {
-
+            System.out.println("No File at location: " + loc);
+            System.exit(0);
         }
 
     }
