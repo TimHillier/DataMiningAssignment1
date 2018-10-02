bce8ab6a7281049cf37f8a5e2db3b8e6f02c6dc1
Tim Hillier <Tim.r.hillier@gmail.com>
2018-10-01 20:35:36 -0700

    Making new found list

--- src/com/company/CandidateGen.java
+++ src/com/company/CandidateGen.java
@@ -1,12 +1,12 @@
 package com.company;
-import java.util.HashMap;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
 
+import java.lang.reflect.Array;
+import java.util.*;
+import java.util.stream.Stream;
 
 public class CandidateGen {
     static int MIN = -1;
+    static String[] found;
 
     //used for setting the minthresh.
     public static void CandidateGen(int _min)
@@ -18,30 +18,20 @@ public class CandidateGen {
 
     public static Block[] Generate(String[][] InputArray)
     {
-        ArrayList<String> found = new ArrayList<String>();  //for searching.
 
-        for(int i = 0; i < InputArray.length;i++)
-        {
-            for(int j = 0; j< InputArray[i].length;j++)
-            {
-                if(!(found.contains(InputArray[i][j])))
-                {
-                    found.add(InputArray[i][j]);
+        found = GenerateSet(InputArray);
 
-                }
-            }
-
-        }
-
-        MIN = found.size() / (100/MIN); // this is trash but I cant math rn.
+        MIN = found.length / (100/MIN); // this is trash but I cant math rn.
 
 
-        HashMap candidates = HashMapTest(found);
+        HashMap candidates = HashMapper(found);
         ElementCounter(InputArray,candidates);
         printmap(candidates);
 //        System.out.println("min " + MIN);
-        Trim(candidates,MIN);
+        Trim(candidates,MIN,found);
+        System.out.println("found" + Arrays.toString(found));
         printmap(candidates);
+        GenerateNext(found);
 
 
         Block[] Out = null;
@@ -75,7 +65,7 @@ public class CandidateGen {
      * @param found A list of unique elements from the data.
      * @return Returns a hashmap of the Unique elements.
      */
-    private static HashMap HashMapTest(ArrayList<String> found)
+    private static HashMap HashMapper(String[] found)
     {
         //will a hashmap work for this?
         HashMap<String,Integer> hmap = new HashMap<String,Integer>();
@@ -88,25 +78,67 @@ public class CandidateGen {
     }
 
     /**
-     *  remove anything below the threshold.
+     * remove anything below the threshold.
+     * Also Trims the found Array.
      * @param HM The HashMap that holds this iteration
      * @param MIN The minimum threashold to search for.
+     * @param FND The found array.
      */
-    private static void Trim(HashMap HM, int MIN)
+    private static void Trim(HashMap HM, int MIN, String[] FND)
     {
+        ArrayList<String> removed = new ArrayList<>();
         for(int i = 0; i < MIN;i++)
         {
+
             HM.values().remove(i); //its slow at higher data thigns
         }
+        System.out.println("Here: " + HM.keySet()); //sends collections take these and make them the new found.
+        String[] s = new String[HM.size()];
+        for(int i = 0; i < HM.size();i++)
+        {
+            //We got em
+            s[i] = HM.keySet().iterator().next().toString();
+            HM.keySet().iterator().remove();
 
+        }
+        found = s;
 
     }
 
 
-    //generate ck+1
-    private static void GenerateNext()
+
+    /**
+     * Generates ck+1
+     */
+    private static void GenerateNext(String[] FND)
     {
 
+    }
+
+
+    /**
+     * Generates a set of elements given a list of transactions.
+     * @param InputArray The array of all transactions.
+     * @return The array that is the set.
+     */
+    private static String[] GenerateSet(String[][] InputArray)
+    {
+        ArrayList<String> found = new ArrayList<String>();  //for searching.
+
+        for(int i = 0; i < InputArray.length;i++)
+        {
+            for(int j = 0; j< InputArray[i].length;j++)
+            {
+                if(!(found.contains(InputArray[i][j])))
+                {
+                    found.add(InputArray[i][j]);
+
+                }
+            }
+
+        }
+         String[] f = found.toArray(new String[found.size()]);
+         return f;
     }
 
 //for testing prints the hashmap
